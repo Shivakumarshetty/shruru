16c42dd5d388b97e66a020cf323ff65e15fa105d
Shiva <shivakumarshettyss@gmail.com>
2022-05-23 22:24:40 +0530

    test

---
+++ src/test/java/gpay/com/AppTest.java
@@ -0,0 +1,20 @@
+package gpay.com;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
